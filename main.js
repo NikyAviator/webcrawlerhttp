f3b9b744b3d71bd53098461bb346b71c869f37b1
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-02-20 21:13:26 +0100

    added jsdom and an comment in main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 // npm init - add to script: "start" "node main.js" -> start with npm start
 // npm install --save-dev jest -> installerar jest som dev paket! -> då skapas node_modules. to run: npm test (ändra i package.json)
+// npm install jsdom
 console.log('hello world!');
