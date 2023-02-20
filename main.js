a6f7bc62da4db87b72f21a7b1417fda388f2b2b7
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-02-20 20:20:41 +0100

    added jest as dev dependency and ignored node_modules

---
+++ main.js
@@ -0,0 +1,3 @@
+// npm init - add to script: "start" "node main.js" -> start with npm start
+// npm install --save-dev jest -> installerar jest som dev paket! -> då skapas node_modules
+console.log('hello world!');
