a745db217f618e93a9d1c6ee4ab571237b1fabc6
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-02-21 16:29:02 +0100

    added checks for invalid URLs (both absolute and relative)

--- crawl.test.js
+++ crawl.test.js
@@ -56,3 +56,35 @@ test('getURLsFromHTML - relative URLs', () => {
   const expected = ['https://blog.boot.dev/path/'];
   expect(actual).toEqual(expected);
 });
+
+test('getURLsFromHTML - Multiple both relative and absolute paths ', () => {
+  const inputHTMLBody = `
+  <hmtl>
+    <body>
+      <a href="/path1/">Boot.dev blog path1 </a>
+      <a href="https://blog.boot.dev/path2/">Boot.dev blog path2</a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = 'https://blog.boot.dev';
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [
+    'https://blog.boot.dev/path1/',
+    'https://blog.boot.dev/path2/',
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test('getURLsFromHTML - invalid URL', () => {
+  const inputHTMLBody = `
+  <hmtl>
+    <body>
+      <a href="invalid">Invalid URL</a>
+    </body>
+  </html>
+  `;
+  const inputBaseURL = 'https://blog.boot.dev';
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
+  expect(actual).toEqual(expected);
+});
